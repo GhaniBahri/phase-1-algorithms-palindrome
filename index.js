3624c56d7d3c242561fe364f143b88ad697bd0a5
GhaniBahri <bahri.ghanou@gmail.com>
2023-09-01 18:08:47 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arr = word.split("")
+  const lastEl= arr.length-1
+  const treshold = Math.floor(arr.length/2)
+  let sum =0 
+    
+  for(let i=0; i<treshold; i++){
+   if(arr[i]=== arr[lastEl-i]){
+    sum ++
+   }
+  }
+  return sum === treshold
 }
 
 /* 
@@ -20,6 +31,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("climmilc"));
 }
 
 module.exports = isPalindrome;
